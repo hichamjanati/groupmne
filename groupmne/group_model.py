[MRG] Fix "rh" and "both" leadfield computation (#15)
* optional to None

* fix right hemi computation + make readable

* add correspondent test

* adjust starting col for right hemi

groupmne/group_model.py
@@ -120,36 +120,52 @@ def _group_filtering(fwds, src_ref, noise_covs=None):
         subject = src[0]["subject_his_id"]
         group_info["subjects"].append(subject)
         ch_names.append(utils._get_channels(fwd, cov))
-        # find removed vertices
+
+        # find mapping between src_ref and new src space of fwd
         mapping = utils.get_morph_src_mapping(src_ref, src)
-        gain = []
+
+        # create a gain with the "large" src_ref resolution
+        gain = np.ones((fwd["nchan"], sum(n_sources)))
         for i in range(2):
+            # pos contains the reference sources of src_ref
             pos = list(mapping[0][i].keys())
             positions[i].append(pos)
             vertno = - np.ones(n_sources[i]).astype(int)
-            gain_h = np.ones((fwd["nchan"], n_sources[i]))
             # re-order columns of the gain matrices
+            # vertno_tmp contains the new vertices in the right order
             vertno_tmp = np.array(list(mapping[0][i].values()))
+            # find the appropriate permutation to reorder columns
             permutation = np.argsort(np.argsort(vertno_tmp))
-            gain_h[:, pos] = fwd["sol"]["data"][:, permutation]
-            vertno[pos] = vertno_tmp
-            gain.append(gain_h)
-            vertices[i].append(vertno)
 
-        gain = np.hstack(gain)
+            # these indices allow to switch between left and right hemispheres
+            col_0 = i * n_sources[0]
+            col_1 = i * fwd["src"][0]["nuse"]
+            full_gain = fwd["sol"]["data"]
+
+            # filter the gain on kept sources
+            gain[:, col_0 + pos] = full_gain[:, col_1 + permutation]
+
+            # add the kept vertices to the list
+            vertno[pos] = vertno_tmp
+            vertices[i].append(vertno)
         gains.append(gain)
 
+    # Now we can compute the intersection of all common vertices
     common_pos_lh = np.array(list(
         set(positions[0][0]).intersection(*positions[0])))
     common_pos_rh = np.array(list(
         set(positions[1][0]).intersection(*positions[1])))
-    common_pos = np.r_[common_pos_lh, common_pos_rh + len(positions[0])]
+
+    common_pos = np.r_[common_pos_lh, common_pos_rh + n_sources[0]]
     vertno_ref = [common_pos_lh, common_pos_rh]
+
+    # Compute the final filtered gains
     for i in range(len(fwds)):
         gains[i] = gains[i][:, common_pos]
         for j, common in enumerate(vertno_ref):
             vertices[j][i] = vertices[j][i][common.astype(int)]
     gains = np.stack(gains, axis=0)
+
     ch_names = set(ch_names[0]).intersection(*ch_names)
     group_info["ch_names"] = list(ch_names)
     group_info["vertno_lh"] = vertices[0]

groupmne/tests/test_group.py
@@ -1,4 +1,4 @@
-from groupmne import compute_gains, compute_inv_data
+from groupmne import compute_gains, compute_inv_data, utils
 import mne
 from mne.datasets import testing
 import numpy as np
@@ -64,7 +64,6 @@ def test_inverse_data(src_fwds):
 @pytest.mark.parametrize("hemi", ["lh", "rh", "both"])
 def test_different_gains(src_fwds, hemi):
     src_ref, fwds = src_fwds
-    # are different
     gains, group_info = compute_gains(fwds, src_ref, ch_type="grad",
                                       hemi=hemi)
     n_ch = len(group_info["sel"])
@@ -77,3 +76,32 @@ def test_different_gains(src_fwds, hemi):
     with pytest.raises(AssertionError):
         gains /= abs(gains).max()
         np.testing.assert_allclose(gains[0], gains[1])
+
+
+@testing.requires_testing_data
+@pytest.mark.parametrize("hemi", ["lh", "rh", "both"])
+def test_filtering_fsaverage(src_fwds, hemi):
+    src_ref, (fwd0, fwd1) = src_fwds
+    fwds = [fwd1, fwd1]
+    gains, group_info = compute_gains(fwds, src_ref, ch_type="grad",
+                                      hemi=hemi)
+
+    n_lh = fwds[0]["src"][0]["nuse"]
+    if hemi == "lh":
+        col0 = 0
+        col1 = n_lh
+    elif hemi == "rh":
+        col0 = n_lh
+        col1 = None
+    elif hemi == "both":
+        col0 = 0
+        col1 = None
+    for fwd, gain in zip(fwds, gains):
+        fwd = mne.convert_forward_solution(fwd, surf_ori=True,
+                                           force_fixed=True,
+                                           use_cps=True)
+        ch_names = utils._get_channels(fwd)
+        sel = utils._filter_channels(fwd["info"], ch_names, "grad")
+        fwd_gain = fwd["sol"]["data"][sel]
+        fwd_gain = fwd_gain[:, col0:col1]
+        np.testing.assert_allclose(gain, fwd_gain)